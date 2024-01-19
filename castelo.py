6f5f63045143e23e8b7f93e0400104f63f1217b1
thiagosoares <thiagocsoares@gmail.com>
2024-01-19 08:12:09 -0300

    Corrigindo main

--- castelo.py
+++ castelo.py
@@ -4,12 +4,11 @@ class Castelo(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
         self.image = pygame.Surface((50, 50))
-        self.image.fill((128, 128, 128))  # Cor cinza para representar o castelo
+        self.image.fill((128, 128, 128))
         self.rect = self.image.get_rect(topleft=(x, y))
-        self.hp = 100  # Pontos de vida do castelo
+        self.hp = 1000
 
     def sofrer_dano(self, dano):
         self.hp -= dano
         if self.hp <= 0:
-            # Implemente aqui a lógica de derrota ou fim de jogo
             pass
